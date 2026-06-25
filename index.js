e5159ae826fafda99843307241361be034b2c585
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-06-26 03:17:35 +0530

    Last commit

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-setInterval(()=>{
-    d=new Date();
-    htime=d.getHours();
-    mtime=d.getMinutes();
-    stime=d.getSeconds();
-hrotation= 30*htime+mtime/2;
-mrotation=6*mtime;
-srotation=6*stime;
-hour.style.transform=`rotate(${hrotation}deg)`;
-minute.style.transform=`rotate(${mrotation}deg)`;
-second.style.transform=`rotate(${srotation}deg)`    
-},1000)
+const hour = document.querySelector("#hour");
+const minute = document.querySelector("#minute");
+const second = document.querySelector("#second");
+
+function updateClock(){
+
+    const now = new Date();
+
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+
+    const hRotation = h * 30 + m / 2;
+    const mRotation = m * 6;
+    const sRotation = s * 6;
+
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    minute.style.transform = `rotate(${mRotation}deg)`;
+    second.style.transform = `rotate(${sRotation}deg)`;
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
